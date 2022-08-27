a1ec04e695a9c8db2f43ec0028025a25ff999bbf
Hesam1999 <Amirhesam_Hashemi@outlook.com>
2022-08-27 21:01:16 +0430

    Part 13 Training Step 3 - this training about using smallest comment in template file

--- myworld/members/views.py
+++ myworld/members/views.py
@@ -188,7 +188,8 @@ def testing(request):
   return HttpResponse(template.render(context, request))
 """
 #Part 13 Training
-# Step 1, 2
+# Step 1, 2, 3
 def testing(request):
   template = loader.get_template('template.html')
+  
   return HttpResponse(template.render())
