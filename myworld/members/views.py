f4dd6f8c4f20b966bfd3702ef10dc4a1fcbaf0f3
Hesam1999 <Amirhesam_Hashemi@outlook.com>
2022-08-27 20:51:35 +0430

    Part 13 Training Step 2 - about using add a comment for another programmer

--- myworld/members/views.py
+++ myworld/members/views.py
@@ -188,7 +188,7 @@ def testing(request):
   return HttpResponse(template.render(context, request))
 """
 #Part 13 Training
-# Step 1
+# Step 1, 2
 def testing(request):
   template = loader.get_template('template.html')
   return HttpResponse(template.render())
