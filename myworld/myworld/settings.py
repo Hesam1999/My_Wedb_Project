105722d22fff74d503e9160ba674706df2355ddf
Amirhesam_Hashemi <Amirhesam_Hashemi@outlook.com>
2022-09-17 20:12:29 +0430

    Part 18 Training Step 1 - about can't found address and I exit this project debug mode and add local address in Allowed_hosts step

--- myworld/myworld/settings.py
+++ myworld/myworld/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-7f=1l)n9mkro4hfbjns-psdeo$0lqj#k0iw0hyo#b&*)mk85q4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
