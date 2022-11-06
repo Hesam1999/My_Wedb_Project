7144595b63b8c2189f94609453fee8c28adf7abe
Amirhesam_Hashemi <Amirhesam_Hashemi@outlook.com>
2022-11-06 22:12:29 +0330

    Part 21 Training Step 9.9 - if template tag - There are some built-in operators you can use when evaluating if statements: is greater than, or equal to

--- myworld/members/views.py
+++ myworld/members/views.py
@@ -581,7 +581,7 @@ def testing(request):
     return HttpResponse(template.render(context, request))
 """
 # Part 21 Training
-# Step 9.4, 9.5, 9.6, 9.7, 9.8
+# Step 9.4, 9.5, 9.6, 9.7, 9.8, 9.9
 
 def testing(request):
     template = loader.get_template('template.html')
