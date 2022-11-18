3429addbaf00cc1a153ea8cb2932c3c4c6e65b7e
Amirhesam_Hashemi <Amirhesam_Hashemi@outlook.com>
2022-11-18 12:59:56 +0330

    Part 21 Training Step 13.1 - now template tag - Outputs the current date/time: Add the current date.

--- myworld/members/views.py
+++ myworld/members/views.py
@@ -699,7 +699,7 @@ def testing(request):
 """
 
 # Part 21 Training
-# Step 11.1, 11.2, 12.1, 12.2
+# Step 11.1, 11.2, 12.1, 12.2, 13.1
 
 def testing(request):
     template = loader.get_template('template.html')
